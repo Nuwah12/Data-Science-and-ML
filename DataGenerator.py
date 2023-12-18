4df65d282b22f8fe264fecd737bfa5efb9f160c1
Nuwah12 <ngb23@pitt.edu>
2023-12-17 20:49:48 -0500

    testing with ols

--- DataGenerator.py
+++ DataGenerator.py
@@ -4,23 +4,31 @@
 ##########
 import numpy as np
 class DataGenerator():
-    def __init__(self, num_samples=1000, seed=None, noise_std=1):
+    def __init__(self, num_samples=1000, num_features=1, seed=None, noise_std=1):
         np.random.seed(seed)
         self.n_samples=num_samples
         self.seed=seed
         self.noise_std=noise_std
-        self.noise = np.random.randn(num_samples, 1) * noise_std # Generate noise from the normal distribtion, times some std
-        self.X = np.random.rand(num_samples, 1) # Generate random X values from uniform distribution [0,1]
+        self.num_features = num_features
+        self.noise = np.random.randn(num_samples) * noise_std # Generate noise from the normal distribtion, times some std
+        self.X = np.random.rand(num_samples, num_features) # Generate random X values from uniform distribution [0,1]
     def __str__(self):
         msg = """
               DataGenerator object::
-              {} samples
+              {} features, {} samples
               seed={}
               noise std={}
-              """.format(self.n_samples, self.seed, self.noise_std)
+              """.format(self.num_features, self.n_samples, self.seed, self.noise_std)
         return msg
-    def generate_linear(self, slope, intercept): # Generate corresponding y values with normally distributed noise
-        y = slope * self.X + intercept + self.noise
+    def generate_linear(self, intercept, weights): # Generate corresponding y values with normally distributed noise
+        if isinstance(weights, list):
+            weights = np.array(weights).astype(float)
+        num_rows = self.X.shape[0]
+        ones_column = np.ones((num_rows))
+        X = np.insert(self.X, 0, ones_column, axis=1)
+        coeffs = np.insert(weights, 0, intercept, axis=0)
+        y = np.dot(X, coeffs)
+        y = y + self.noise
         return self.X,y
     def generate_exponential(self, base, exponent):
         y = base ** (exponent * self.X) + self.noise
@@ -31,4 +39,15 @@ class DataGenerator():
     def generate_sinusoidal(self, amplitude, frequency, phase):
          y = amplitude * np.sin(2 * np.pi * frequency * self.X + phase) + self.noise
          return self.X,y
+def test():
+    dg = DataGenerator(num_samples=2500, num_features=3, seed=42)
+    l = dg.generate_linear(intercept=8.5,weights=[5,7,10])
+    from Regression.LinearRegression import LinearRegressionMethods
+    print(l[0])
+    print(l[1].shape)
+    lr = LinearRegressionMethods(l[0], l[1])
+    p = lr.ols()
+    print(p)
+if __name__ == "__main__":
+    test()
 
