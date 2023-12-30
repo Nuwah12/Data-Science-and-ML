ea931bf05274b57866ee4a00f8723fd105ed48dc
Nuwah12 <nburget1@hotmail.com>
2023-12-30 18:41:31 -0500

    L2 regularization and subclassed reg. regression

--- Regression/RegularizedRegression.py
+++ Regression/RegularizedRegression.py
@@ -5,16 +5,20 @@
 import numpy as np
 import time
 from .LinearRegression import LinearRegressionMethods
-class L1RegularizedRegressionMethods(LinearRegressionMethods):
+class RegularizedRegressionMethods(LinearRegressionMethods):
+    def __init__(self, X, y, L1Lambda=0, L2Lambda=0):
+        super().__init__(X, y)
+        self.L1 = L1Lambda 
+        self.L2 = L2Lambda 
     def __str__(self):
         msg = """
-              L1-Regularized Linear Regression Object. Available methods:
+              Regularized Linear Regression Object. Available methods:
               Gradient Descent w/ Mean Squared Error (MSE) - minimizes MSE via gradient descent
               X = {}
               y = {}
               """.format(self.X, self.y)
         return msg
-    def mse_gradient_descent(self, num_iters=500, learning_rate=0.05, threshold=None, L1_lambda=0.5):
+    def mse_gradient_descent(self, num_iters=500, learning_rate=0.05, threshold=None):
         """
         Finds ideal weights for a matrix (self.X) and target value (self.y) assuming a linear relationship y=w1x1 + w2x2 + ... xnxn
 
@@ -34,15 +38,17 @@ class L1RegularizedRegressionMethods(LinearRegressionMethods):
             # Keep track of residuals in the object 
             self.residuals = np.abs(yhat - self.y)
             # Calculate regularization 
-            l1_reg = L1_lambda * np.sum(np.abs(self.weights))
-            # Calculate gradients with respect to weight vector and L1 regularization
+            l1_reg = self.L1 * np.sum(np.abs(self.weights))
+            l2_reg = self.L2 * np.sum(self.weights**2)
+            # Calculate gradients with respect to weight vector and L1/L2 regularization
             weight_gradient = (1/len(self.X)) * np.dot(self.X.T, (yhat - self.y))
-            l1_gradient = L1_lambda * np.sign(self.weights)
+            l1_gradient = self.L1 * np.sign(self.weights)
+            l2_gradient = 2 * self.L2 * self.weights
             # Update weights with added L1 reg. gradient and bias
-            self.weights -= learning_rate * weight_gradient + learning_rate * l1_gradient
+            self.weights -= learning_rate * weight_gradient + learning_rate * l1_gradient + learning_rate * l2_gradient
             self.bias -= learning_rate * np.mean(yhat - self.y)
-            # Calculate cost (loss, the Mean Squared Error, PLUS the L1 regularization term)
-            cost = np.mean((yhat - self.y)**2) + l1_reg
+            # Calculate cost (loss, the Mean Squared Error, PLUS the L1 and L2 regularization terms)
+            cost = np.mean((yhat - self.y)**2) + l1_reg + l2_reg
             # If convergance threshold was specified:
             if threshold is None:
                 continue
